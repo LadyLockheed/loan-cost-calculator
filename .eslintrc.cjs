0d42a8269d022cd9f8e16b172ab3b742ee22e21b
Karin Karlsson <karin.drake@gmail.com>
2023-07-16 18:55:41 +0200

    Tried to get the format in order

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,11 +7,13 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    "prettier"
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'react'],
   rules: {
+    "react/react-in-jsx-scope": 0,
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
