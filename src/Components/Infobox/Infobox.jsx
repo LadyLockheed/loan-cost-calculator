7773057df2b604f5991e8f087833c9498688ae21
Karin Karlsson <karin.drake@gmail.com>
2023-07-17 17:35:49 +0200

    Added component Infobox

---
+++ src/Components/Infobox/Infobox.jsx
@@ -0,0 +1,18 @@
+import './InfoboxStyle.css'
+
+const Infobox = (props) => {
+
+    const { amountPerMonth } = props
+
+    return (
+        <>
+        <div className='box'>
+            <p className='text-headline'>Example of monthly cost</p>
+            <p className='text-amount'>{amountPerMonth} SEK/month</p>
+            <div className='arrow'></div>
+        </div>
+        </>
+    )
+}
+
+export default Infobox
